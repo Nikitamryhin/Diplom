3b30194e68e79b3117fc7e64d89c37eeb12b2544
Никита <149968216+Nikitamryhin@users.noreply.github.com>
2025-03-17 22:30:20 +0300

    Delete diplom directory

--- diplom/it_asset_manager/models/department.py
+++
@@ -1,7 +0,0 @@
-class Department:
-    def __init__(self, id=None, name=None):
-        self.id = id
-        self.name = name
-
-    def __repr__(self):
-        return f"Department(id={self.id}, name='{self.name}')"
--- diplom/it_asset_manager/models/employee.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    def __init__(self, id, name, position):
-        self.id = id
-        self.name = name
-        self.position = position
-
-    def __repr__(self):
-        return f"Employee(id={self.id}, name='{self.name}')"
